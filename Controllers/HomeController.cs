180250b56877765c42682d78580d2899c92ba444
Emily Bender <bender.emily1@gmail.com>
2020-11-08 20:54:49 -0500

    App setup, started models & DbContext

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServerAppFinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Emily Bender";
+            ViewBag.Item = "Chocolate Chip Cookie";
+            ViewBag.Difficulty = 1; 
+            return View();
+        }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServerAppFinalProject.Models
+{
+    public class Recipe
+    {
+        //EF Core will configure the database to generate this value
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+        public string Origin { get; set;  }
+    }
+}
