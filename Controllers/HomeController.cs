8b6647a67afee8178a4d17f90c232dc9041f238b
Emily Bender <bender.emily1@gmail.com>
2020-11-09 23:40:34 -0500

    Added Resources and About Me pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebServerAppFinalProject.Models;
 
 namespace WebServerAppFinalProject.Controllers
 {
     public class HomeController : Controller
     {
+        private RecipeContext context { get; set; }
+
+        public HomeController(RecipeContext ctx)
+        {
+            context = ctx; 
+        }
         public IActionResult Index()
         {
-            ViewBag.Name = "Emily Bender";
-            ViewBag.Item = "Chocolate Chip Cookie";
-            ViewBag.Difficulty = 1; 
+            var recipes = context.Recipes.Include(r => r.Season)
+                .Include(r => r.Category)
+                .OrderBy(r => r.Name).ToList();
+            return View(recipes);
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult Resources()
+        {
             return View();
         }
     }
